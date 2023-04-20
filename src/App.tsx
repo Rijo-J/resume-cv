02e94c4192c38a219001e48c9e6c815a1df1a324
JRijo <99342891+John-Stiff@users.noreply.github.com>
2023-04-20 13:20:17 +0100

    chore: dev-dep: sass + eslint

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
-import './App.css';
+import React from 'react';
+import './App.scss';
 
 function App() {
-  return (
-    <div className="App">
-      <h1>Hello World!</h1>
-    </div>
-  );
+    return (
+        <div className="App">
+            <h1>Hello World!</h1>
+        </div>
+    );
 }
 
 export default App;
